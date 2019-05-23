9afe7ae866b8dc20cc06a1465280674d929a4c5d
frilli31 <luca.all1996@gmail.com>
2019-05-23 15:17:10 +0200

    Ex4: Added printing of graph

--- ex4clustering/src/main/java/Main.java
+++ ex4clustering/src/main/java/Main.java
@@ -1,12 +1,25 @@
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.BubbleChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.demo.charts.ExampleChart;
+
+import java.io.IOException;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<County> cities = Parser.get("unifiedCancerData_212");
-        System.out.println(cities.size());
+        Set<County> cities_212 = Parser.get("unifiedCancerData_212");
+        Set<County> cities_562 = Parser.get("unifiedCancerData_562");
+        Set<County> cities_1041 = Parser.get("unifiedCancerData_1041");
+        Set<County> cities_3108 = Parser.get("unifiedCancerData_3108");
 
-        System.out.println(HierarchicalClustering.run(cities, 15));
-
-        System.out.println(ClusteringKMeans.run(cities, 15, 5));
+        ExampleChart<BubbleChart> exampleChart = new BubbleChart01(HierarchicalClustering.run(cities_212, 15));
+        BubbleChart chart = exampleChart.getChart();
+        try {
+            BitmapEncoder.saveBitmapWithDPI(chart, "./Sample_Chart_300_DPI", BitmapEncoder.BitmapFormat.JPG, 300);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new SwingWrapper<BubbleChart>(chart).displayChart();
     }
 }
---
+++ ex4clustering/src/main/java/chart.java
@@ -0,0 +1,52 @@
+import org.knowm.xchart.BubbleChart;
+import org.knowm.xchart.BubbleChartBuilder;
+import org.knowm.xchart.demo.charts.ExampleChart;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Basic Bubble Chart
+ * <p>
+ * Demonstrates the following:
+ * <ul>
+ * <li>Bubble Chart
+ */
+class BubbleChart01 implements ExampleChart<BubbleChart> {
+
+    Set<Cluster> clusters;
+
+    BubbleChart01(Set<Cluster> clusters) {
+        this.clusters = clusters;
+    }
+
+
+    @Override
+    public BubbleChart getChart() {
+
+        // Create Chart
+        BubbleChart chart = new BubbleChartBuilder().width(1200).height(900).title("BubbleChart01").xAxisTitle("X").yAxisTitle("Y").build();
+
+        List<Cluster> clusters2 = new ArrayList<>(clusters);
+
+        for (int i = 0; i < clusters.size(); i++) {
+            ArrayList<Double> xData = new ArrayList<>();
+            ArrayList<Double> yData = new ArrayList<>();
+            ArrayList<Double> population = new ArrayList<>();
+
+            Cluster cluster = clusters2.get(i);
+
+            for (County county : cluster.cities) {
+                xData.add(county.getX());
+                yData.add(county.getY());
+                population.add((double) county.getPopulation() / 100000);
+            }
+
+            chart.addSeries("" + i, xData.stream().mapToDouble(z -> z).toArray(), yData.stream().mapToDouble(z -> z).toArray(), population.stream().mapToDouble(z -> z).toArray());
+        }
+
+        return chart;
+    }
+
+}
